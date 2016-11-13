ec82e5e4b3b8e52004aff77b962dae0e0fb87fc5
Colin Douch <colin@quirl.co.nz>
2016-11-13 21:19:04 +1300

    Added vault install to image

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,3 +9,15 @@ package{ 'zip':
 package{ 'awscli':
   ensure => 'installed'
 }
+
+remote_file { '/tmp/vault.zip':
+  ensure => present,
+  source => 'https://releases.hashicorp.com/vault/0.6.2/vault_0.6.2_linux_amd64.zip',
+}
+
+exec { 'unzip':
+  command     => 'sudo unzip /tmp/vault.zip -d /usr/bin',
+  creates     => '/usr/bin/vault',
+  path        => ['/usr/bin'],
+  refreshonly => true,
+}
